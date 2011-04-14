ef20c52fbf957cf0412c20875907d5384f1bd670
Michel Megens <dev.michelm@gmail.com>
2011-04-15 01:23:09 +0200

    A20 line is working now

--- fenix/boot/x86/loader.asm
+++ fenix/boot/x86/loader.asm
@@ -25,13 +25,25 @@ main:
 	mov si, hello
 	call println
 
+	call enable_A20
+	call check_a20
+
 ; setup GDT, A20 line
 
+	or ax,ax
+	jz panic ; ax = 0 means no A20
+
+succes:
 	mov si, loaded
 	call println
+	jmp $
 	
 ; switch to protected mode and 32 bits
 ;	call kernelmain
+
+panic:
+	mov si, error
+	call println
 	jmp $
 
 ;
@@ -60,13 +72,115 @@ newline:
 
 endprintln:
 	xor bh, bh
-	mov al, 0x08
+	mov al, 0x08 ; backspace
 	inc dx
 	int 0x10
 	cmp cx, dx
-	jne endprintln
+	jne endprintln ; not at beginning yet
 
-	retn	
+	retn
+
+;
+; enable A20 line
+;
+
+enable_A20:
+        cli
+ 
+        call    a20wait
+        mov     al,0xAD
+        out     0x64,al
+ 
+        call    a20wait
+        mov     al,0xD0
+        out     0x64,al
+ 
+        call    a20wait2
+        in      al,0x60
+        push    eax
+ 
+        call    a20wait
+        mov     al,0xD1
+        out     0x64,al
+ 
+        call    a20wait
+        pop     eax
+        or      al,2
+        out     0x60,al
+ 
+        call    a20wait
+        mov     al,0xAE
+        out     0x64,al
+ 
+        call    a20wait
+        sti
+        ret
+ 
+a20wait:
+        in      al,0x64
+        test    al,2
+        jnz     a20wait
+        ret
+ 
+ 
+a20wait2:
+        in      al,0x64
+        test    al,1
+        jz      a20wait2
+        ret
+
+;
+; check_A20
+;
+
+check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+ 
+    cli
+ 
+    xor ax, ax ; ax = 0
+    mov es, ax
+ 
+    not ax ; ax = 0xFFFF
+    mov ds, ax
+ 
+    mov di, 0x0500
+    mov si, 0x0510
+ 
+    mov al, byte [es:di]
+    push ax
+ 
+    mov al, byte [ds:si]
+    push ax
+ 
+    mov byte [es:di], 0x00
+    mov byte [ds:si], 0xFF
+ 
+    cmp byte [es:di], 0xFF
+ 
+    pop ax
+    mov byte [ds:si], al
+ 
+    pop ax
+    mov byte [es:di], al
+ 
+    mov ax, 0
+    je check_a20__exit
+ 
+    mov ax, 1
+ 
+check_a20__exit:
+    pop si
+    pop di
+    pop es
+    pop ds
+    popf
+    sti
+    ret
 
 ;
 ; Some sort of data segment
@@ -74,6 +188,7 @@ endprintln:
 
 	hello db 'Loading Golden Eagle bootloader', 0x0
 	loaded db 'Golden Eagle loaded - Calling Fenix kernel', 0x0
+	error db 'Failed to load Golden Eagle succesfull...', 0x0
 ;
 ; End
 ;
