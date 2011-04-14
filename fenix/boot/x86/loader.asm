bd88e32330be58edcace4a4b3ba1708ba17d306b
Michel Megens <dev.michelm@gmail.com>
2011-04-14 23:55:58 +0200

    implemented some sort of hello world bootloader

--- fenix/boot/x86/loader.asm
+++ fenix/boot/x86/loader.asm
@@ -19,7 +19,19 @@
 [BITS 16]
 [ORG 0X7C00]
 
+; EXTERN kernelmain
+
 main:
+	mov si, hello
+	call println
+
+; setup GDT, A20 line
+
+	mov si, loaded
+	call println
+	
+; switch to protected mode and 32 bits
+;	call kernelmain
 	jmp $
 
 ;
@@ -28,8 +40,9 @@ main:
 
 println:
 	xor cx, cx ; strlen
+	xor dx, dx
 
-startprint:
+putchar:
 	lodsb
 	or al, al
 	jz newline 			; 0 byte found
@@ -37,6 +50,7 @@ startprint:
 	xor bh, bh			; page 0
 	int 0x10
 	inc cx
+	jmp putchar
 
 newline:
 	xor bh, bh
@@ -47,7 +61,22 @@ newline:
 endprintln:
 	xor bh, bh
 	mov al, 0x08
+	inc dx
+	int 0x10
+	cmp cx, dx
+	jne endprintln
 
+	retn	
+
+;
+; Some sort of data segment
+;
+
+	hello db 'Loading Golden Eagle bootloader', 0x0
+	loaded db 'Golden Eagle loaded - Calling Fenix kernel', 0x0
+;
+; End
+;
 
 times 512-($-$$)-2 db 0
 dw 0xAA55
