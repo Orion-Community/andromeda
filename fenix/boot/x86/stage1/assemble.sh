891543acfee1b7db8ebed8d6caaf36e631105bff
Michel Megens <dev.michelm@gmail.com>
2011-04-22 11:42:43 +0200

    Cleaned up the project.

--- fenix/boot/x86/stage1/assemble.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copy first sector
-
-dd if=loader.bin of=goldeneaglebl.bin seek=0
-
-# Copy second sector
-
-dd if=stage2.bin of=goldeneaglebl.bin seek=1
--- fenix/boot/x86/stage1/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo /sbin/losetup /dev/loop0 loader.bin
-sudo bochs -f bochsrc.txt
-sudo /sbin/losetup -d /dev/loop0
