4e3567f33dffaf5224718b0a0360c6c3025ccc2b
Michel Megens <dev.michelm@gmail.com>
2011-05-01 15:25:10 -0700

    Fix in git ignore file.

--- boot/x86/stage2/pmodemain.asm
+++ boot/x86/stage2/pmodemain.asm
@@ -2,9 +2,9 @@
 [EXTERN kmain]
 [SECTION .text]
 
-start:
+pmodemain:
 ; 	mov byte [ds:0xB8002], 'P'      ; Move the ASCII-code of 'P' into first video memory
 ;       mov byte [ds:0xB8003], 1Bh      ; Assign a color code
 	;int 0x19
-	;call kmain
+	call kmain
 	jmp $
