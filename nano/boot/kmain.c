327ab3dd38f8c314d5065830733eddb89d71f58a
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2011-07-06 10:39:44 +0200

    Removed a testing line from the production

--- nano/boot/kmain.c
+++ nano/boot/kmain.c
@@ -170,8 +170,6 @@ int kmain(unsigned long magic, multiboot_info_t* hdr)
   printf("End test\n");
   #endif
   
-  printf("%i\n", atoi("1024"));
-  
   for (;;) // Infinite loop, to make the kernel schedule when there is nothing to do
   {
     printf("You can now shutdown your PC\n");
