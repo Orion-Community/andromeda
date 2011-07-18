2e92f3551928d2aa4319caaa5666ecf93aa6fc4d
Michel Megens <dev.michelm@gmail.com>
2011-07-19 00:29:29 +0200

    Changed ide header dramaticaly.

--- arch/x86/diskio/ide.h
+++ arch/x86/diskio/ide.h
@@ -26,100 +26,56 @@
 #define ATA_SR_IDX     0x02
 #define ATA_SR_ERR     0x01
 
-/* Some error code definitions. */
-#define ATA_ER_BBK      0x80
-#define ATA_ER_UNC      0x40
-#define ATA_ER_MC       0x20
-#define ATA_ER_IDNF     0x10
-#define ATA_ER_MCR      0x08
-#define ATA_ER_ABRT     0x04
-#define ATA_ER_TK0NF    0x02
-#define ATA_ER_AMNF     0x01
+/* ATA I/O registers */
+#define OL_MASTER_ATA_BASE 0x1f0
 
-/* Command definitions */
-#define ATA_CMD_READ_PIO          0x20
-#define ATA_CMD_READ_PIO_EXT      0x24
-#define ATA_CMD_READ_DMA          0xC8
-#define ATA_CMD_READ_DMA_EXT      0x25
-#define ATA_CMD_WRITE_PIO         0x30
-#define ATA_CMD_WRITE_PIO_EXT     0x34
-#define ATA_CMD_WRITE_DMA         0xCA
-#define ATA_CMD_WRITE_DMA_EXT     0x35
-#define ATA_CMD_CACHE_FLUSH       0xE7
-#define ATA_CMD_CACHE_FLUSH_EXT   0xEA
-#define ATA_CMD_PACKET            0xA0
-#define ATA_CMD_IDENTIFY_PACKET   0xA1
-#define ATA_CMD_IDENTIFY          0xEC
+#define OL_MASTER_ATA_DATA 		OL_MASTER_ATA_BASE
 
-// ATAPI only commands:
-#define      ATAPI_CMD_READ       0xA8
-#define      ATAPI_CMD_EJECT      0x1B
+#define OL_MASTER_ATA_ERR_INFO 		OL_MASTER_ATA_BASE+1
+#define OL_MASTER_ATA_FEATURES		OL_MASTER_ATA_ERR_INFO
 
-/* IDE MASTER/SLAVE definitions */
-#define IDE_ATA        0x00
-#define IDE_ATAPI      0x01
-#define ATA_MASTER     0x00
-#define ATA_SLAVE      0x01
+#define OL_MASTER_ATA_SECTOR_COUNT 	OL_MASTER_ATA_BASE+2
+#define OL_MASTER_ATA_LOW_LBA 		OL_MASTER_ATA_BASE+3
+#define OL_MASTER_ATA_MID_LBA		OL_MASTER_ATA_BASE+4
+#define OL_MASTER_ATA_HIGH_LBA		OL_MASTER_ATA_BASE+5
 
-/* I/O registers */
-#define ATA_REG_DATA       0x00
-#define ATA_REG_ERROR      0x01
-#define ATA_REG_FEATURES   0x01
-#define ATA_REG_SECCOUNT0  0x02
-#define ATA_REG_LBA0       0x03
-#define ATA_REG_LBA1       0x04
-#define ATA_REG_LBA2       0x05
-#define ATA_REG_HDDEVSEL   0x06
-#define ATA_REG_COMMAND    0x07
-#define ATA_REG_STATUS     0x07
-#define ATA_REG_SECCOUNT1  0x08
-#define ATA_REG_LBA3       0x09
-#define ATA_REG_LBA4       0x0A
-#define ATA_REG_LBA5       0x0B
-#define ATA_REG_CONTROL    0x0C
-#define ATA_REG_ALTSTATUS  0x0C
-#define ATA_REG_DEVADDRESS 0x0D
+#define OL_MASTER_ATA_DRIVE_SELECT	OL_MASTER_ATA_BASE+6
+#define OL_MASTER_ATA_HEAD_SELECT	OL_MASTER_ATA_DRIVE_SELECT
 
-/* Channels */
-#define      ATA_PRIMARY      0x00
-#define      ATA_SECONDARY    0x01
- 
-/* Directions */
-#define      ATA_READ      0x00
-#define      ATA_WRITE     0x01
+#define OL_MASTER_ATA_COMMAND		OL_MASTER_ATA_BASE+7
+#define OL_MASTER_ATA_STATUS		OL_MASTER_ATA_COMMAND
 
-// channel structures - master/slave
-struct IDEChannelRegisters {
-	uint16_t base;  // I/O Base.
-	uint16_t ctrl;  // Control Base
-	uint16_t bmide; // Bus Master IDE
-	uint8_t  nIEN;  // nIEN (No Interrupt);
-} channels[2];
+/* ATA PIO MODE commands */
+#define OL_READ_LBA28_SECTORS 0x20
+#define OL_READ_LBA48_SECTORS 0x24
 
-// Max 4 drives
-struct ide_device {
-	uint8_t reserved;    // 0 (Empty) or 1 (This Drive really exists).
-	uint8_t channel;     // 0 (Primary Channel) or 1 (Secondary Channel).
-	uint8_t drive;       // 0 (Master Drive) or 1 (Slave Drive).
-	uint16_t type;        // 0: ATA, 1:ATAPI.
-	uint16_t signature;   // Drive Signature
-	uint16_t capabilities;// Features.
-	uint32_t commandSets; // Command Sets Supported.
-	uint32_t size;        // Size in Sectors.
-	uint8_t model[41];   // Model in string.
-} ide_devices[4];
+// Max 4 partitions
+struct partition_table 
+{
+	uint8_t boot_ind;
+	uint8_t starting_head;
+	uint16_t starting_sect_cyl; // bits 0-5 starting sector - bits 6-15 starting cylinder
+	uint8_t sys_id;
+	uint8_t ending_head;
+	uint16_t ending_sect_cyl; // same story as byte 2 and 3
+	uint32_t lba;
+	uint32_t total_sectors; // total sectors in this partition
+} bootdrive[4];
+typedef struct partition_table OL_ptable
 
 /**
- * Read from a channel
+ * Read from drive.
+ * 
+ * The relative lba value is the lba offset of the partition which contain the sectors.
+ * 
+ * NOTE: You !CANNOT! read outside a partition, even if those sectors exist.
  */
-uint8_t ide_read(uint8_t chan, uint8_t reg);
+extern uint8_t ide_read(uint32_t sectors /* sectors to read */, uint32_t buffer /* destination buffer */, 
+		OL_ptable * partition /* partition where the sectors are found in */, uint32_t lba /* relative lba */);
 
 /**
- * Write to a channel
+ * Initialize the ide driver. It will set the partition table correctly.
+ * 
+ * NOTE: This function expects that the current partition tables are located at 0x0:0x7c00.
  */
-void ide_write(uint8_t chan, uint8_t reg, uint8_t data);
-
-/**
- * Read the buffer
- */
-void ide_read_buffer(uint8_t chan, uint8_t reg, uint32_t buf, uint32_t quads);
+extern OL_ptable * ide_init();
