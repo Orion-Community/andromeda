97464e8919f0db90ed7394f8416007f9dd0646ef
Steven v.d. Schoot <stevenvdschoot@hotmail.com>
2011-07-22 16:54:48 +0200

    updated debug script

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd `dirname $0`/..
+
+function error { 
+echo Failed. 
+ls -l $SNAPSHOT | mail seanw -s "Backup script failed" 
+exit 
+}
+
 FLAGS="-O3"
 DEFS=""
 for i in $*
@@ -8,4 +15,6 @@ do
     DEFS="$DEFS $i=enabled"
 done
 make clean -B $DEFS
-make FLAGS="$FLAGS" CC=distcc -B $DEFS
+make FLAGS="$FLAGS" CC=distcc $DEFS
+RET=$?
+exit $RET
--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd `dirname $0`
 ./compile.sh $@
+RET=$?
+if [ ${RET} -eq 0 ]; then
 ./updatefloppy.sh
 kvm -fda floppy.img -m 32M -s
+else
+echo " "
+echo -en '\E[1;31m'"\033[1m                ***   $RET Errors found!   ***\033[0m"
+echo " "
+fi
 
