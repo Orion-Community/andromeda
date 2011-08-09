15433b5486b3907e5ae959845dfa39dfd778a36a
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2011-08-09 22:30:01 +0200

    Did some work on the fs

--- fs/vfs/fs.c
+++ fs/vfs/fs.c
@@ -27,11 +27,24 @@ struct _FS_INODE* _fs_root = NULL;
 #define _FS_BMP_OFF(a) (a%_FS_BMP_BITS)
 #define _FS_BMP_SZ(a)  ((a%_FS_BMP_BITS == 0) ? a/_FS_BMP_BITS : a/_FS_BMP_BITS+1)
 
+/*
+ * /proc
+ * /dev
+ */
+
 void fsInit(inode_t* root)
 {
   if (root == NULL)
   {
     panic("No root file system supplied!");
+    _fs_root = kalloc(sizeof(inode_t));
+    _fs_root -> inode = _FS_ROOT_INODE;
+    _fs_root -> size  = 2*sizeof(struct _FS_DIR_ENTRY);
+    _fs_root -> data  = kalloc(sizeof(struct _FS_FILE));
+    _fs_root -> data -> start = kalloc (_fs_root -> size);
+    _fs_root -> data -> end = (char*)((unsigned long) _fs_root -> data -> start + _fs_root -> size);
+    _fs_root -> data -> read = _fs_root -> data -> start;
+    _fs_root -> data -> write = _fs_root -> data -> start;
   }
   else
   {
--- include/fs/file.h
+++ include/fs/file.h
@@ -28,7 +28,7 @@ struct _FS_FILE
   char* read;  // The read pointer (within this inode)
   char* write; // The write pointer (within this inode, should be >= read)
   size_t size; // Size of this inode
-  struct _IO_FILE* chain; // Next inode, if this one isn't large enough
+  struct _FS_FILE* chain; // Next inode, if this one isn't large enough
   char* path;  // Path to file
   boolean buffered; // TRUE for buffered
 };
@@ -37,8 +37,10 @@ typedef struct _FS_FILE FILE;
 
 struct _FS_DIR_ENTRY
 {
-  char* name; 
-  struct _FS_INODE* inode;
+  char name[255];
+  unsigned int drv;
+  unsigned int inode;
+  struct _FS_INODE* virt_inode;
 };
 
 #endif
