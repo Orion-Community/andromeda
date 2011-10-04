ee7d75ee9f6ce7cf33b05e7fd684ff58d0ed792d
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2011-10-04 10:42:32 +0200

    Did a little clean-up

--- src/include/mm/map.h
+++ src/include/mm/map.h
@@ -64,8 +64,8 @@ struct page
 extern module_t modules[];
 extern size_t memsize;
 
-uint32_t map_alloc_page(uint32_t list_idx);
-uint32_t map_rm_page(uint32_t page_index);
+addr_t map_alloc_page(addr_t list_idx);
+addr_t map_rm_page(addr_t page_index);
 
 int build_map(multiboot_memory_map_t*, int);
 
