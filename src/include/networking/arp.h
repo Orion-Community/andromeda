f9f8421f6e53eda9c75c3809a9cd55fcff9a9153
Michel Megens <dev@michelmegens.net>
2011-12-12 11:05:35 +0100

    net: make a arp setup.

---
+++ src/include/networking/arp.h
@@ -0,0 +1,45 @@
+/*
+ *   Andromeda Project - Address Resolution Protocol
+ *   Copyright (C) 2011  Michel Megens
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ARP_H
+#define __ARP_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct arp
+{
+  uint16_t hwtype; /* hardware type */
+  uint16_t ptype; /* protocol type */
+  uint8_t hlength; /* hardware address length */
+  uint8_t plength; /* protocol address length */
+  uint16_t opcode; /* operation code */
+  uint8_t * src_hw; /* source hardware address */
+  uint8_t * src_pr; /* source protocol */
+  uint8_t * dst_hw; /* destination hardware address */
+  uint8_t * dst_pr; /* destination protocol */
+} *arp_t;
+
+static struct arp *alloc_arp_structure(struct arp *);
+static void free_arp_structure(struct arp *);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ src/networking/arp.c
@@ -0,0 +1,55 @@
+/*
+ *   Andromeda Project - Address Resolution Protocol
+ *   Copyright (C) 2011  Michel Megens - dev@michelmegens.net
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <networking/arp.h>
+
+/**
+ * Allocate memory in the arp structure for the source & destination hardware
+ * address/protocol
+ */
+static struct arp *
+alloc_arp_structure(struct arp *arp)
+{
+  if(arp == NULL)
+    return NULL;
+
+  arp->src_hw = kalloc(sizeof(uint8_t)*arp->hlength);
+  arp->src_pr = kalloc(sizeof(uint8_t)*arp->plength);
+  arp->dst_hw = kalloc(sizeof(uint8_t)*arp->hlength);
+  arp->dst_pr = kalloc(sizeof(uint8_t)*arp->plength);
+  return arp;
+}
+
+/**
+ * Free's all allocated memory in the given arp structure.
+ *
+ * @arg arp The arp structure which should be free'd
+ */
+static void
+free_arp_structure(struct arp *arp)
+{
+  if(arp == NULL)
+    return;
+  
+  free(arp->src_hw);
+  free(arp->src_pr);
+  free(arp->dst_hw);
+  free(arp->dst_pr);
+  free(arp);
+}
