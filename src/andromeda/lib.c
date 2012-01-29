45805ccba89220e99e982577e55dfa979fa78d07
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2012-01-29 15:53:52 +0100

    lib: Added stubs for list management functions

---
+++ src/andromeda/lib.c
@@ -0,0 +1,67 @@
+/*
+ *  Andromeda
+ *  Copyright (C) 2011  Bart Kuivenhoven
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <thread.h>
+#include <andromeda/core.h>
+
+struct list*
+list_init()
+{
+        return NULL;
+}
+
+int
+list_destroy()
+{
+        return -E_NOFUNCTION;
+}
+
+int
+list_add_head()
+{
+        return -E_NOFUNCTION;
+}
+
+int list_rm_element()
+{
+        return -E_NOFUNCTION;
+}
+
+int list_insert_element()
+{
+        return -E_NOFUNCTION;
+}
+
+int list_add_tail()
+{
+        return -E_NOFUNCTION;
+}
+
+struct list_node*
+list_get_element()
+{
+        return NULL;
+}
+
+struct list_node*
+list_next()
+{
+        return NULL;
+}
