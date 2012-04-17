d4db0245dd24398be3a2ec2a79de0707d00d9b2e
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2012-04-17 09:26:43 +0200

    math: Fixed an ownership issue.
    Steven van der Schoot copied the copyright header over but forgot to give the
    credit for the code written by him, to himself. Issue is now fixed.

--- src/math/math.c
+++ src/math/math.c
@@ -1,6 +1,6 @@
 /*
-    Orion OS, The educational operatingsystem
-    Copyright (C) 2011  Bart Kuivenhoven
+    Andromeda
+    Copyright (C) 2011  Steven van der Schoot
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
