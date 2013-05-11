a3bd60eee168f02f26d871ab2e753add04ecb7af
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2013-05-11 22:40:11 +0200

    Test: Add qemu-kvm for added realism
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 export KERN_PATH=bin/andromeda.img
+export MEM_SIZE="64M"
 
-if ! command -v qemu-system-i386; then
-	qemu -kernel $KERN_PATH -m 64M -monitor stdio;
+export QEMU_FLAGS="-kernel $KERN_PATH -m $MEM_SIZE -monitor stdio"
+
+if ! command -v qemu-kvm; then
+	if ! command -v qemu-system-i386; then
+		qemu $QEMU_FLAGS
+	else
+		qemu-system-i386 $QEMU_FLAGS
+	fi;
 else
-	qemu-system-i386 -kernel $KERN_PATH -m 64M -monitor stdio ;
+	qemu-kvm $QEMU_FLAGS
 fi
--- scripts/qemu_iso.sh
+++ scripts/qemu_iso.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 export KERN_PATH=bin/andromeda.iso
+export MEM_SIZE="64M"
+export QEMU_FLAGS="-cdrom $KERN_PATH -monitor stdio -m $MEM_SIZE"
 
-if ! command -v qemu-system-i386 &> /dev/null; then
-	qemu -cdrom $KERN_PATH -monitor stdio -m 64M
+if ! command -v qemu-kvm &> /dev/null; then
+	if ! command -v qemu-system-i386 &> /dev/null; then
+		qemu -cdrom $QEMU_FLAGS
+	else
+		qemu-system-i386 $QEMU_FLAGS
+	fi;
 else
-	qemu-system-i386 -cdrom $KERN_PATH -monitor stdio -m 64M
-fi
+	qemu-kvm $QEMU_FLAGS
+fi;
