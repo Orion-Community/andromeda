acedd3df20f035120400fb181530f813df06d9ca
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2013-05-16 16:02:13 +0200

    qemu: For Debian machines, preffer KVM over qemu
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -5,10 +5,14 @@ export MEM_SIZE="64M"
 export QEMU_FLAGS="-kernel $KERN_PATH -m $MEM_SIZE -monitor stdio"
 
 if ! command -v qemu-kvm; then
-	if ! command -v qemu-system-i386; then
-		qemu $QEMU_FLAGS
+	if ! command -v kvm; then
+		if ! command -v qemu-system-i386; then
+			qemu $QEMU_FLAGS
+		else
+			qemu-system-i386 $QEMU_FLAGS
+		fi;
 	else
-		qemu-system-i386 $QEMU_FLAGS
+		kvm $QEMU_FLAGS
 	fi;
 else
 	qemu-kvm $QEMU_FLAGS
--- scripts/qemu_iso.sh
+++ scripts/qemu_iso.sh
@@ -3,12 +3,16 @@ export KERN_PATH=bin/andromeda.iso
 export MEM_SIZE="64M"
 export QEMU_FLAGS="-cdrom $KERN_PATH -monitor stdio -m $MEM_SIZE"
 
-if ! command -v qemu-kvm &> /dev/null; then
-	if ! command -v qemu-system-i386 &> /dev/null; then
-		qemu -cdrom $QEMU_FLAGS
-	else
-		qemu-system-i386 $QEMU_FLAGS
-	fi;
-else
-	qemu-kvm $QEMU_FLAGS
+if ! command -v qemu-kvm; then                                                  
+        if ! command -v kvm; then                                               
+                if ! command -v qemu-system-i386; then                          
+                        qemu $QEMU_FLAGS                                        
+                else                                                            
+                        qemu-system-i386 $QEMU_FLAGS                            
+                fi;                                                             
+        else                                                                    
+                kvm $QEMU_FLAGS                                                 
+        fi;                                                                     
+else                                                                            
+        qemu-kvm $QEMU_FLAGS                                                    
 fi;
