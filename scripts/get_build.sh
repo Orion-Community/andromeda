a46e1cb5568e84b4069377a5b140359d48c47ff0
Bart Kuivenhoven <bemkuivenhoven@gmail.com>
2014-09-26 20:54:13 +0200

    build: Fix broken symlink bug
    If the build binary is available, but the symlink is not yet present,
    the kernel will fail to configure and build. By moving the code for
    making the symlink away from the condition in which the binary has to be
    fetched, we eliminate the possibility of this bug.
    
    Signed-off-by: Bart Kuivenhoven <bemkuivenhoven@gmail.com>

--- scripts/get_build.sh
+++ scripts/get_build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "Fetching build system"
+#Fetch build itself
 BUILD_VERSION=-0.2
 if [ ! -f scripts/build$BUILD_VERSION ]; then
 	wget http://ci.orion-os.eu/job/Build$BUILD_VERSION/lastSuccessfulBuild/artifact/build -O scripts/build$BUILD_VERSION;
-	if [ -f scripts/build ]; then
-		rm scripts/build
-	fi
-	ln -sv build$BUILD_VERSION scripts/build
 	chmod +x scripts/build$BUILD_VERSION
 fi
+
+#Make sure the symlink is correct
+if [ -f scripts/build ]; then
+	rm scripts/build
+fi
+ln -sv build$BUILD_VERSION scripts/build
